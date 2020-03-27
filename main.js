da0b87b9e6db43da11f1a2c2b40a4173d117a7b2
Sarthak-Rout <sarthakr97@gmail.com>
2020-03-28 01:35:42 +0530

    add chartjs

--- main.js
+++ main.js
@@ -146,3 +146,23 @@ document.addEventListener('click', (evt) => {
     dropdown.classList.remove('open');
   }
 });
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'fhfh', 'fhfjg', 'hfhghf'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45, 10, 20, 15 ]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
