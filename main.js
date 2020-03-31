c7529a7b3a39aace30472f3e1affa262e4d2afa6
Biswabijaya Samal <me@biswabijaya.com>
2020-04-01 04:02:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,15 +20,18 @@
 //         });
 //     });
 
-  
+
 //   });
+
+
 
 var xlabel = [];
 var ylabel = [];
 
-function getchart(ylabel){
-    var xlabel = ['Total cases', 'Active cases', 'Total deaths', 'Total recovered'];
+console.log(ylabel);
 
+
+var xlabel = ['Total cases', 'Active cases', 'Total deaths', 'Total recovered'];
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -38,7 +41,7 @@ var chart = new Chart(ctx, {
     data: {
         labels: xlabel ,
         datasets: [{
-            label: 'My First dataset',
+            label: 'Country',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
             data: ylabel
@@ -48,10 +51,6 @@ var chart = new Chart(ctx, {
     // Configuration options go here
     options: {}
 });
-
-
-};
-
 
 $(document).ready(function(){
     $('#countryname').on('submit', function(e){
@@ -63,7 +62,7 @@ $(document).ready(function(){
         //     date.push(ymd(i));
 
         // }
-        console.log('hello');
+
 
         //country data
         var settings = {
@@ -77,21 +76,23 @@ $(document).ready(function(){
             }
         }
 
-        
+
         $.ajax(settings).done(function (response) {
             var obj = JSON.parse(response);
+            ylabel = [];
             $('.country-details').css('display', 'inline')
-            $('#countryid').append(obj.latest_stat_by_country[0].id);
             $('#con-name').append(obj.latest_stat_by_country[0].country_name);
             $('#total-cases').append(obj.latest_stat_by_country[0].total_cases);
             $('#active-cases').append(obj.latest_stat_by_country[0].active_cases);
             $('#total-deaths').append(obj.latest_stat_by_country[0].total_deaths);
             $('#total-recover').append(obj.latest_stat_by_country[0].total_recovered);
-            ylabel.push(obj.latest_stat_by_country[0].total_cases);
-            ylabel.push(obj.latest_stat_by_country[0].active_cases);
-            ylabel.push(obj.latest_stat_by_country[0].total_deaths);
-            ylabel.push(obj.latest_stat_by_country[0].total_recovered);
-
+            ylabel.push(parseInt(obj.latest_stat_by_country[0].total_cases));
+            ylabel.push(parseInt(obj.latest_stat_by_country[0].active_cases));
+            ylabel.push(parseInt(obj.latest_stat_by_country[0].total_deaths));
+            ylabel.push(parseInt(obj.latest_stat_by_country[0].total_recovered));
+            chart.data.datasets[0].label = obj.latest_stat_by_country[0].country_name;
+            chart.data.datasets[0].data = ylabel;
+            chart.update();
         });
             $('#countryid').empty();
             $('#con-name').empty();
@@ -99,8 +100,6 @@ $(document).ready(function(){
             $('#active-cases').empty();
             $('#total-deaths').empty();
             $('#total-recover').empty();
-
-            getchart(ylabel);
 
      //date wise data
     // for (let index = date.length; index >= 0; index--) {
@@ -115,12 +114,12 @@ $(document).ready(function(){
     //             "x-rapidapi-key": "fce61b9f8amshf1817269f8a2a63p1d2382jsn44ad90697b6d"
     //         }
     //     }
-        
+
     //     $.ajax(settings3).done(function (response) {
-    //         var obj3 = JSON.parse(response);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    //         var obj3 = JSON.parse(response);
     //         datewisecases.push(obj3.stat_by_country[0].total_cases)
     //     });
-        
+
     // }
 //     var ctx = document.getElementById('myChart').getContext('2d');
 //     var chart = new Chart(ctx, {
@@ -141,8 +140,8 @@ $(document).ready(function(){
 //     // Configuration options go here
 //     options: {}
 // });
-    
-        
+
+
     });
 
     //world data
@@ -156,7 +155,7 @@ $(document).ready(function(){
             "x-rapidapi-key": "fce61b9f8amshf1817269f8a2a63p1d2382jsn44ad90697b6d"
         }
     }
-    
+
     $.ajax(settings2).done(function (response) {
         var obj2 = JSON.parse(response);
         $('#world-total-cases').append("Total cases: " + obj2.total_cases);
@@ -168,9 +167,9 @@ $(document).ready(function(){
 
     });
 
-    
 
-  
+
+
   });
 
 // const inputField = document.querySelector('.chosen-value');
